b82a219dec5d9921ea30e6765764797c77e0b8a8
halfa <youngyukari@gmail.com>
2015-09-28 11:01:44 +0200

    Basic server with multiple clients management

--- server.pl
+++ server.pl
@@ -1,35 +1,73 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl -w
 use strict;
 
 package Server;
 
-sub listen () {
-use Socket;
+sub loop () {
+  use Socket;
+  use IO::Select;
 
-    my $sock;
-    my $port = 6667; #irc port
+  $| = 1;
+  my($proto, $port , $sock , $s , @ready , $so , $addrinfo , $client , $inp);
 
-    socket($sock, AF_INET, SOCK_STREAM, getprotobyname('tcp')) or die $!;
-    
-    #bind soket descriptor to local port
-    bind($sock , sockaddr_in($port, INADDR_ANY))
-        or  die "bind failed : $!";
-    #accept incoming connections
-    while(1)
-    {
-        my($client);
-        my $addrinfo = accept($client , $sock);
-     
-        my($port, $iaddr) = sockaddr_in($addrinfo);
-        my $name = gethostbyaddr($iaddr, AF_INET);
-     
-        print "Connection accepted from $name : $port \n";
-     
-        #send some message to the client
-        print $client "Hello client how are you\n";
-    }
- 
-    close($sock);
+  $proto = getprotobyname('tcp');
+
+  #create a socket handle (descriptor)
+  socket($sock, AF_INET, SOCK_STREAM, $proto)
+      or die "could not create socket : $!";
+
+  #bind to local port passed as arg
+  $port = "6667";
+  bind($sock , sockaddr_in($port, INADDR_ANY))
+      or  die "bind failed : $!";
+
+  listen($sock , 10);
+  print "Server is now listening on port ".$port."...\n";
+
+  #accept incoming connections and talk to clients
+  $s = IO::Select->new();
+
+  $s->add($sock);
+
+  while(1)
+  {
+      @ready = $s->can_read(0);
+
+      foreach $so(@ready)
+      {
+          #new connection read
+          if($so == $sock)
+          {
+              my($client);
+              $addrinfo = accept($client , $sock);
+
+              my($port, $iaddr) = sockaddr_in($addrinfo);
+              my $name = gethostbyaddr($iaddr, AF_INET);
+
+              print "Connection accepted from $name : $port \n";
+
+              #send some message to the client
+              send($client , "Hello client how are you\n" , 0);
+
+              $s->add($client);
+          }
+
+          # existing client read
+          else
+          {
+              chop($inp = <$so>);
+              chop($inp);
+              print "Received -- $inp \n";
+
+              #send reply back to client
+              send($so , "OK : $inp\n" , 0);
+          }
+      }
+  }
+
+  #close the socket
+  close($sock);
+  exit(0);
 }
 
-listen();
+loop();
