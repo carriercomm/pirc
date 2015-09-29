96a2c948cef76565f2d72915acd09aa7ffe19979
halfa <youngyukari@gmail.com>
2015-09-29 11:45:01 +0200

    Refactor code and indent

--- server.pl
+++ server.pl
@@ -3,71 +3,78 @@ use strict;
 
 package Server;
 
-sub loop () {
-  use Socket;
-  use IO::Select;
+#initialize network layer
+sub initNetwork () {
+    use Socket;
+    use IO::Select;
 
-  $| = 1;
-  my($proto, $port , $sock , $s , @ready , $so , $addrinfo , $client , $inp);
+    $| = 1;
+    my($proto, $port , $sock);
 
-  $proto = getprotobyname('tcp');
+    $proto = getprotobyname('tcp');
 
-  #create a socket handle (descriptor)
-  socket($sock, AF_INET, SOCK_STREAM, $proto)
-      or die "could not create socket : $!";
+    #create a socket handle (descriptor)
+    socket($sock, AF_INET, SOCK_STREAM, $proto)
+        or die "could not create socket : $!";
 
-  #bind to local port passed as arg
-  $port = "6667";
-  bind($sock , sockaddr_in($port, INADDR_ANY))
-      or  die "bind failed : $!";
+    #bind to local port passed as arg
+    $port = "6667";
+    bind($sock , sockaddr_in($port, INADDR_ANY))
+        or  die "bind failed : $!";
 
-  listen($sock , 10);
-  print "Server is now listening on port ".$port."...\n";
+    listen($sock , 10); # open a socket for
+    print "Server is now listening on port ".$port."...\n";
 
-  #accept incoming connections and talk to clients
-  $s = IO::Select->new();
+    #start accepting incoming connections
+    connectionsLoop($sock);
 
-  $s->add($sock);
-
-  while(1)
-  {
-      @ready = $s->can_read(0);
-
-      foreach $so(@ready)
-      {
-          #new connection read
-          if($so == $sock)
-          {
-              my($client);
-              $addrinfo = accept($client , $sock);
-
-              my($port, $iaddr) = sockaddr_in($addrinfo);
-              my $name = gethostbyaddr($iaddr, AF_INET);
-
-              print "Connection accepted from $name : $port \n";
-
-              #send some message to the client
-              send($client , "Hello client how are you\n" , 0);
-
-              $s->add($client);
-          }
-
-          # existing client read
-          else
-          {
-              chop($inp = <$so>);
-              chop($inp);
-              print "Received -- $inp \n";
-
-              #send reply back to client
-              send($so , "OK : $inp\n" , 0);
-          }
-      }
-  }
-
-  #close the socket
-  close($sock);
-  exit(0);
+    #close the socket
+    close($sock);
+    exit(0);
 }
 
-loop();
+sub connectionsLoop ($sock) {
+    my($s , @ready , $so , $addrinfo , $client , $inp);
+    #accept incoming connections and talk to clients
+    $s = IO::Select->new();
+
+    $s->add($sock);
+
+    while(1)
+    {
+        @ready = $s->can_read(0);
+
+        foreach $so(@ready)
+        {
+            #new connection read
+            if($so == $sock)
+            {
+                my($client);
+                $addrinfo = accept($client , $sock);
+
+                my($port, $iaddr) = sockaddr_in($addrinfo);
+                my $name = gethostbyaddr($iaddr, AF_INET);
+
+                print "Connection accepted from $name : $port \n";
+
+                #send some message to the client
+                #send($client , "Hello client how are you\n" , 0);
+
+                $s->add($client);
+            }
+
+            # existing client read
+            else
+            {
+                chop($inp = <$so>);
+                chop($inp);
+                print "Received -- $inp \n";
+
+                #send reply back to client
+                #send($so , "OK : $inp\n" , 0);
+            }
+        }
+    }
+}
+
+initNetwork();
